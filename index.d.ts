ade126afdd7d518c7b4ac91c0be35a85ed501b55
Max Haze <eng.mahan.hazrati@gmail.com>
2020-10-18 16:26:44 +0200

    add nvmrc file and typescript support

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export function getUrl(): string
+export function slashDivider(): string[]
+export function getProtocol(): string
+export function domainName(): string
+export function domainWithProtocol(): string
+export function sitePort(): string
+export function searchQuery(): string
+export function pathName(): string
+export function hashAddress(): string
+export function StringIndexFinder(nameOfString: string): string
+export function isEncrypted(): string
