0a8688091dba9595c51bfd6d813094bcc1ff6a9f
Pooya <pouyapanahandeh@users.noreply.github.com>
2025-01-06 11:52:57 +0000

    update package with testing

--- index.js
+++ index.js
@@ -1,141 +1,105 @@
-// Import punycode for IDN support
-const punycode = require('punycode');
+// Import URL module for IDN support
+const url = require('url');
+
+// Validate if a string is a valid URL
+const isValidUrl = (urlString = '') => {
+  try {
+    const parsedUrl = new URL(urlString);
+    return parsedUrl.protocol.startsWith('http') && parsedUrl.hostname !== '';
+  } catch {
+    return false;
+  }
+};
 
 // Retrieve the full URL
-const getUrl = (url = '') => url;
+const getUrl = (urlString = '') => urlString;
 
 // Divide URL based on "/"
-const slashDivider = (url = '') => url.split('/');
+const slashDivider = (urlString = '') => urlString.split('/');
 
 // Get protocol of the URL
-const getProtocol = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.protocol;
-  } catch {
-    return 'Invalid URL';
-  }
+const getProtocol = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  return new URL(urlString).protocol;
 };
 
 // Get domain name
-const domainName = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.hostname;
-  } catch {
-    return 'Invalid URL';
-  }
+const domainName = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  return new URL(urlString).hostname;
 };
 
 // Get domain with protocol
-const domainWithProtocol = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return `${parsedUrl.protocol}//${parsedUrl.hostname}`;
-  } catch {
-    return 'Invalid URL';
-  }
+const domainWithProtocol = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  return `${parsedUrl.protocol}//${parsedUrl.hostname}`;
 };
 
 // Get site port
-const sitePort = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.port ? `Port number is: ${parsedUrl.port}` : "Couldn't find port";
-  } catch {
-    return 'Invalid URL';
-  }
+const sitePort = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  return parsedUrl.port ? `Port number is: ${parsedUrl.port}` : "Couldn't find port";
 };
 
 // Get search query
-const searchQuery = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.search ? parsedUrl.search : "Search query doesn't exist";
-  } catch {
-    return 'Invalid URL';
-  }
+const searchQuery = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  return parsedUrl.search ? parsedUrl.search : "Search query doesn't exist";
 };
 
 // Get path name
-const pathName = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.pathname ? parsedUrl.pathname : "Path name doesn't exist";
-  } catch {
-    return 'Invalid URL';
-  }
+const pathName = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  return parsedUrl.pathname ? parsedUrl.pathname : "Path name doesn't exist";
 };
 
 // Get hash address
-const hashAddress = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.hash ? parsedUrl.hash : "Hash address doesn't exist";
-  } catch {
-    return 'Invalid URL';
-  }
+const hashAddress = (urlString = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  return parsedUrl.hash ? parsedUrl.hash : "Hash address doesn't exist";
 };
 
 // Find specific string index in URL
-const stringIndexFinder = (url = '', nameOfString = '') => {
-  const startingIndex = url.indexOf(nameOfString);
+const stringIndexFinder = (urlString = '', nameOfString = '') => {
+  const startingIndex = urlString.indexOf(nameOfString);
   if (startingIndex === -1) return 'String not found';
   const endIndex = startingIndex + nameOfString.length - 1;
   return `From index ${startingIndex}, end index ${endIndex}`;
 };
 
 // Check if the URL is encrypted
-const isEncrypted = (url = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.protocol === 'https:';
-  } catch {
-    return false;
-  }
-};
-
-// Validate if a string is a valid URL
-const isValidUrl = (url = '') => {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
+const isEncrypted = (urlString = '') => {
+  if (!isValidUrl(urlString)) return false;
+  return new URL(urlString).protocol === 'https:';
 };
 
 // Add query parameter to URL
-const addQueryParam = (url = '', key = '', value = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    parsedUrl.searchParams.append(key, value);
-    return parsedUrl.toString();
-  } catch {
-    return 'Invalid URL';
-  }
+const addQueryParam = (urlString = '', key = '', value = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  parsedUrl.searchParams.append(key, value);
+  return parsedUrl.toString();
 };
 
 // Remove query parameter from URL
-const removeQueryParam = (url = '', key = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    parsedUrl.searchParams.delete(key);
-    return parsedUrl.toString();
-  } catch {
-    return 'Invalid URL';
-  }
+const removeQueryParam = (urlString = '', key = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  parsedUrl.searchParams.delete(key);
+  return parsedUrl.toString();
 };
 
 // Update query parameter in URL
-const updateQueryParam = (url = '', key = '', value = '') => {
-  try {
-    const parsedUrl = new URL(url);
-    parsedUrl.searchParams.set(key, value);
-    return parsedUrl.toString();
-  } catch {
-    return 'Invalid URL';
-  }
+const updateQueryParam = (urlString = '', key = '', value = '') => {
+  if (!isValidUrl(urlString)) return 'Invalid URL';
+  const parsedUrl = new URL(urlString);
+  parsedUrl.searchParams.set(key, value);
+  return parsedUrl.toString();
 };
 
 // Encode URL component
@@ -145,10 +109,10 @@ const encodeUrlComponent = (component = '') => encodeURIComponent(component);
 const decodeUrlComponent = (component = '') => decodeURIComponent(component);
 
 // Encode internationalized domain name (IDN)
-const encodeIDN = (domain = '') => punycode.toASCII(domain);
+const encodeIDN = (domain = '') => url.domainToASCII(domain);
 
 // Decode internationalized domain name (IDN)
-const decodeIDN = (domain = '') => punycode.toUnicode(domain);
+const decodeIDN = (domain = '') => url.domainToUnicode(domain);
 
 module.exports = {
   getUrl,
